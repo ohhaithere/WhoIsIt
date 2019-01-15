4636b07ecb45f6fcad931e5af977fc5e87c68741
frostymaster <frostmasterwork@gmail.com>
2019-01-16 00:18:35 +0300

    faces train

--- faces-train.py
+++ faces-train.py
@@ -1,12 +1,14 @@
-import os
-from PIL import Image
-import numpy as np
 import cv2
-
-face_cascade = cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt2.xml")
+import os
+import numpy as np
+from PIL import Image
+import pickle
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
 label_ids = {}
@@ -14,26 +16,36 @@ y_labels = []
 x_train = []
 
 for root, dirs, files in os.walk(image_dir):
-    for file in files:
-        if file.endswith("png"):
-            path = os.path.join(root, file)
-            label = os.path.basename(os.path.dirname(path))
-            if label in label_ids:
-                pass
-            else:
-                label_ids[label] = current_id
-                current_id += 1
-            id_ = label_ids[label]
-            #y_labels.append(label)
-            #x_train.append(path)
-            pil_image = Image.open(path).convert("L")
-            image_array = np.array(pil_image, "uint8")
-            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
-            print(image_array)
+	for file in files:
+		if file.endswith("png") or file.endswith("jpg"):
+			path = os.path.join(root, file)
+			label = os.path.basename(root).replace(" ", "-").lower()
+			#print(label, path)
+			if not label in label_ids:
+				label_ids[label] = current_id
+				current_id += 1
+			id_ = label_ids[label]
+			#print(label_ids)
+			#y_labels.append(label) # some number
+			#x_train.append(path) # verify this image, turn into a NUMPY arrray, GRAY
+			pil_image = Image.open(path).convert("L") # grayscale
+			size = (550, 550)
+			final_image = pil_image.resize(size, Image.ANTIALIAS)
+			image_array = np.array(final_image, "uint8")
+			#print(image_array)
+			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
-            for x,y,w,h in faces:
-                roi = image_array[y:y+h, x:x+w]
-                x_train.append(roi)
-                y_labels.append(id_)
+			for (x,y,w,h) in faces:
+				roi = image_array[y:y+h, x:x+w]
+				x_train.append(roi)
+				y_labels.append(id_)
 
-print(y_labels, x_train)
+
+#print(y_labels)
+#print(x_train)
+
+with open("pickles/face-labels.pickle", 'wb') as f:
+	pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("recognizers/face-trainner.yml")
